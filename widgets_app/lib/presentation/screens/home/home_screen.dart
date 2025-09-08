c9a50e55d963fb4dedd5f53a4272ec8156266794
jcastroe1000 <tonoescom@gmail.com>
2025-09-07 20:33:59 -0600

    go router config

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Flutter + widgets ')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter + widgets ')),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+        return _CustomeListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomeListTile extends StatelessWidget {
+  const _CustomeListTile({required this.menuItem});
+
+  final MenuItems menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () {
+        context.push(menuItem.link);
+      },
+    );
   }
 }
