1b1de55b7ba035fadccf5ceba267f19cc7566587
jcastroe1000 <tonoescom@gmail.com>
2025-10-05 17:30:19 -0600

    adding call to show list movies

--- cinemapedia/lib/presentation/screens/movies/home_screeen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screeen.dart
@@ -1,4 +1,6 @@
+import 'package:cinemapedia/presentation/screens/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreeen extends StatelessWidget {
   static const name = 'home-screen';
@@ -6,6 +8,34 @@ class HomeScreeen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Placeholder());
+    return Scaffold(body: _HomeView());
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingsMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingsMoviesProvider);
+    if (nowPlayingMovies.length == 0) return CircularProgressIndicator();
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(title: Text(movie.title));
+      },
+    );
   }
 }
