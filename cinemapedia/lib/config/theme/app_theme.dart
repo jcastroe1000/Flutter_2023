f4d35e3a1c03467ea9f0ff011ccca72c33a5bd96
jcastroe1000 <tonoescom@gmail.com>
2025-10-11 18:45:43 -0600

    modifyng color and adding new provider

--- cinemapedia/lib/config/theme/app_theme.dart
+++ cinemapedia/lib/config/theme/app_theme.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   ThemeData getTheme() =>
-      ThemeData(useMaterial3: true, colorSchemeSeed: Color(0xff2862F5));
+      ThemeData(useMaterial3: true, colorSchemeSeed: Colors.deepOrange);
 }
