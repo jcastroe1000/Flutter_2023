236f08d1828daf96ecd5c2d4575985a60b86d38f
jcastroe1000 <tonoescom@gmail.com>
2025-10-19 18:07:44 -0600

    adding actor model

---
+++ cinemapedia/lib/domain/entities/actor.dart
@@ -0,0 +1,13 @@
+class Actor {
+  final int id;
+  final String namel;
+  final String profilePath;
+  final String? character;
+
+  Actor({
+    required this.id,
+    required this.namel,
+    required this.profilePath,
+    required this.character,
+  });
+}
